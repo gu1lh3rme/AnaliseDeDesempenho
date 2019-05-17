98c91db7e35eb137dbbf1b14786deb5328ec1363
gu1lh3rme <a14012@bcc.unifal-mg.edu.br>
2019-05-17 17:58:11 -0300

    Little

--- main.c
+++ main.c
@@ -17,6 +17,14 @@
 #include <time.h>
 #include "fila.h"
 
+typedef struct little_ {
+    double tempo_anterior;
+    double soma_areas;
+    double qtd_pacotes;
+} little;
+
+
+
 //Numero aleatorio entre 0 e 1
 
 double aleatorio() {
@@ -65,9 +73,15 @@ double gera_tam_pct() {
  * @return  menor dentre os valores
  */
 double minimo(double a, double b) {
-    if (a < b)
+    if (a <= b)
         return a;
     return b;
+}
+
+void inicia_little(little *l){
+    l->qtd_pacotes = 0.0;
+    l->soma_areas = 0.0;
+    l->tempo_anterior = 0.0;
 }
 
 int main(int argc, char** argv) {
@@ -75,6 +89,16 @@ int main(int argc, char** argv) {
     pacote **fim = malloc(sizeof (pacote *));
     *inicio = NULL;
     *fim = NULL;
+    
+    //variavel para en
+    little en;
+    //variável para ew chegada
+    little ew_chegada;
+    //variável para ew saída
+    little ew_saida;
+    inicia_little(&en);
+    inicia_little(&ew_chegada);
+    inicia_little(&ew_saida);
 
     //iniciando a semente para a geração
     //dos números pseudoaleatorios
@@ -113,13 +137,13 @@ int main(int argc, char** argv) {
     //Fila, onde fila == 0 indica roteador vazio, fila == 1
     //indica 1 pacote, já em transmissão;
     //fila > 1 indica 1 pacote em transmissão e demais em espera
-    double fila = 0.0;
+    // double fila = 0.0;
 
     //tempo de chegada do proximo pacote
     //ao sistema
     double chegada_proximo_pct = chegada_pct(intervalo);
     //printf("Chegada do primeiro pacote: %lF\n", chegada_proximo_pct);
-    double chegada_proximo_pct_cbr;
+    double chegada_proximo_pct_cbr = 0.02;
 
     double saida_pct_atendimento = 0.0;
     double ocupacao = 0.0;
@@ -151,6 +175,15 @@ int main(int argc, char** argv) {
             //gerar o tempo de chegada do próximo
             chegada_proximo_pct = tempo + chegada_pct(intervalo);
 
+            //cálculo little -- E[N]
+            en.soma_areas += en.qtd_pacotes * (tempo - en.tempo_anterior);
+            en.qtd_pacotes++;
+            en.tempo_anterior = tempo;
+            
+            ew_chegada.soma_areas += ew_chegada.qtd_pacotes * (tempo - ew_chegada.tempo_anterior);
+            ew_chegada.qtd_pacotes++;
+            ew_chegada.tempo_anterior = tempo;
+
         } else if (tempo == chegada_proximo_pct_cbr) {
             //chega pct cbr 1200 bytes
             tam_pct = (1200.0 * 8.0) / (1000000.0);
@@ -165,31 +198,78 @@ int main(int argc, char** argv) {
             inserir(inicio, fim, tam_pct);
             //gerar o tempo de chegada do próximo
             chegada_proximo_pct_cbr += 0.02;
-        }
-        else { //saida de pacote
+
+            //cálculo little  --- E[N]
+            en.soma_areas += en.qtd_pacotes * (tempo - en.tempo_anterior);
+            en.qtd_pacotes++;
+            en.tempo_anterior = tempo;
+            
+            //cálculo little  --- E[W] chegada
+            ew_chegada.soma_areas += ew_chegada.qtd_pacotes * (tempo - ew_chegada.tempo_anterior);
+            ew_chegada.qtd_pacotes++;
+            ew_chegada.tempo_anterior = tempo;
+        } else { //saida de pacote
             //    printf("Saída de pacote no tempo: %lF\n", tempo);
             remover(inicio);
 
-            if (inicio != NULL) {
-                //Obtem o tamanho do pacote
+            if (*inicio != NULL) {
+                //Obtem o tamanho do pacotehttps://github.com/gu1lh3rme/AnaliseDeDesempenho.git
                 tam_pct = (*inicio)->tamanho;
                 //gerando o tempo em que o pacote atual sairá do sistema
                 saida_pct_atendimento = tempo + tam_pct / link;
 
                 ocupacao += saida_pct_atendimento - tempo;
             }
+            //cálculo little -- E[N]
+            en.soma_areas += en.qtd_pacotes * (tempo - en.tempo_anterior);
+            en.qtd_pacotes--;
+            en.tempo_anterior = tempo;
+            
+            //cálculo little  --- E[W] saída
+            ew_saida.soma_areas += ew_saida.qtd_pacotes * (tempo - ew_saida.tempo_anterior);
+            ew_saida.qtd_pacotes++;
+            ew_saida.tempo_anterior = tempo;
+            
         }
         //  printf("===========================================\n\n");
         //   getchar();
     }
-
+    
+    ew_saida.soma_areas += ew_saida.qtd_pacotes * (tempo - ew_saida.tempo_anterior);
+    ew_chegada.soma_areas += ew_chegada.qtd_pacotes * (tempo - ew_chegada.tempo_anterior);
+    
+    double en_final = en.soma_areas/tempo;
+    double ew = ew_chegada.soma_areas - ew_saida.soma_areas;
+    ew = ew/ew_chegada.qtd_pacotes;
+    
+    double lambda = ew_chegada.qtd_pacotes / tempo;
+    
     printf("Ocupacao: %lF\n", ocupacao / tempo);
+    printf("\n===========little===============\n");
+    printf("E[N] = %lF\n", en_final);
+    printf("E[W] = %lF\n", ew);
+    printf("Lambda = %lF\n", lambda);
+    printf("\n=======================\n");
+    printf("Validação little: %.10lF\n", en_final - (lambda * ew));
     /*
         printf("Pacotes gerados: %lF\n", cont_pcts);
         printf("Media do intervalo: %lF\n", tempo / cont_pcts);
         printf("Proporção de pacotes com tamanho 550: %lF\n", cont_pct_550 / cont_pcts);
         printf("Proporção de pacotes com tamanho 40: %lF\n", cont_pct_40 / cont_pcts);
         printf("Proporção de pacotes com tamanho 1500: %lF\n", cont_pct_1500 / cont_pcts);
+     */
+
+    /*
+     Saída:
+    Semente: 1556915527
+    Informe o tempo total de simulação: 1000                    
+    Informe o intervalo médio de tempo (segundos) entre pacotes: 0.000441
+    Tamanho do link (Mbps): 10
+    Ocupacao: 0.847439
+
+===========little===============
+E[N] = 5.148129
+
      */
     return (EXIT_SUCCESS);
 }
